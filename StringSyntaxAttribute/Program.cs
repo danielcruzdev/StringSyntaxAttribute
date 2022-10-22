8508b6d6b1f7dc96052657b966d9e2e99fe656f4
Daniel Cruz <d.afc98@gmail.com>
2022-10-21 23:39:02 -0300

    implementing string syntax

--- StringSyntaxAttribute/Program.cs
+++ StringSyntaxAttribute/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+
+TestRegex(@"^a\d+|b\w*");
+TestDateTime(DateTime.Now, "dd/MM/yyyy");
+TestJson("""
+    [
+        {
+            "Name": "Daniel",
+            "Age": 24
+        },
+        {
+           "Name": "Maria",
+           "Age": 62
+        }
+    ]
+    """);
+
+string TestJson([StringSyntax(StringSyntaxAttribute.Json)] string json) => json;
+
+string TestDateTime(DateTime datetime, [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format) => datetime.ToString(format);
+
+string TestRegex([StringSyntax(StringSyntaxAttribute.Regex)] string regex) => regex;
